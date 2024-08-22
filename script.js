f9014bf9190fed828c27eead7afa908400f50a79
MD ARIF <130835470+syedarif786@users.noreply.github.com>
2024-08-22 21:39:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const logDiv = document.getElementById("log");
+const stateDiv = document.getElementById("state");
+const startBtn = document.getElementById("start-btn");
+const stopBtn = document.getElementById("stop-btn");
+
+startBtn.addEventListener('click',()=>{
+    document.addEventListener('keydown',handleDown);
+    document.addEventListener('keyup',handleUp);
+    startBtn.disabled=true;
+    stopBtn.disabled=false;
+});
+
+stopBtn.addEventListener('click',()=>{
+   logDiv.textContent = "";
+    stateDiv.textContent = "";
+    startBtn.disabled=false;
+    stopBtn.disabled=true;
+
+});
+
+function handleDown(e){
+    logDiv.textContent = `key '${e.key}' pressed down`;
+    stateDiv.textContent = 'key is pressed'
+}
+
+function handleUp(e){
+    logDiv.textContent = `key '${e.key}' pressed up`;
+
+    const h2Element = document.createElement('h2')
+    stateDiv.textContent = `key is released`;
+    
+    
+}
+
